11ef4cbac661fc9af0de19a99c1b9e10cff39d78
Amogh Sinha <amoghsinha1121@gmail.com>
2021-03-27 23:52:10 +0530

    PHASE 3. Before Complete start over as I found out: Not allowed to use GUI forms

---
+++ src/main/ui/AddUserPage.java
@@ -0,0 +1,60 @@
+package ui;
+
+import model.Bill;
+import model.User;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class AddUserPage extends JFrame {
+
+    private Bill bill;
+    private JPanel addUserPanel;
+    private JLabel titleLabel;
+    private JLabel nameLabel;
+
+    private JTextField userNameTextField;
+
+    private JButton backButton;
+    private JButton addUserButton;
+
+    public AddUserPage(Bill bill) {
+        this.bill = bill;
+        add(addUserPanel);
+        setTitle("Add-User Page");
+        setSize(400,500);
+
+        addUserButtonPressed(bill);
+        backButtonPressed();
+
+    }
+
+    private void addUserButtonPressed(Bill bill) {
+        addUserButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String s = userNameTextField.getText();
+
+                if (s.equals("")) {
+                    JOptionPane.showMessageDialog(addUserPanel, "Please enter a name");
+                } else {
+                    String userName = userNameTextField.getText();
+                    User user = new User(userName);
+                    bill.getUsersList().addUserToList(user);
+                }
+            }
+        });
+    }
+
+    private void backButtonPressed() {
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                UsersRelated usersRelated = new UsersRelated(bill);
+                usersRelated.setVisible(true);
+                dispose();
+            }
+        });
+    }
+}
--- src/main/ui/UsersRelated.java
+++ src/main/ui/UsersRelated.java
@@ -6,37 +6,49 @@ import model.UsersList;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class UsersRelated extends JFrame {
     // Variables declaration
-
     private JPanel usersPanel;
     private javax.swing.JLabel label1;
+    private Bill bill;
+
     private javax.swing.JTable tblUsers;
     private DefaultTableModel defaultTable;
-    private JButton backButton;
-    private JButton removeUserButton;
-    private JButton addUserButton;
 
+    private JButton addUserButton;
+    private JButton removeUserButton;
+    private JButton backButton;
 
     public UsersRelated(Bill bill) {
-        add(usersPanel);
+        getContentPane().add(usersPanel);
+//        getContentPane().setLayout(new BorderLayout());
+
+//        add(usersPanel);
         setTitle("User Menu");
         setSize(400,500);
+        this.bill = bill;
+
+        initializeTable();
 
         showUsers(bill);
-//        addUserButtonPressed(bill);
+        addUserButtonPressed(bill);
 //        removeUserButtonPressed(bill);
-//        backButtonPressed();
+        backButtonPressed();
+
+
     }
 
     private void initializeTable() {
-        tblUsers = new JTable();
+//        tblUsers = new JTable();
         defaultTable = (DefaultTableModel) tblUsers.getModel();
+        tblUsers.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
 
     }
 
-    @SuppressWarnings("checkstyle:ArrayTypeStyle")
     private void showUsers(Bill bill) {
 
         UsersList usersList = bill.getUsersList();
@@ -45,25 +57,36 @@ public class UsersRelated extends JFrame {
             System.out.println(user.toString());
             String[] o = {user.getName(), String.valueOf(user.getBalance())};
             defaultTable.addRow(o);
+            System.out.println("Done " + i);
+            System.out.println(defaultTable.getRowCount());
 
-            int[] integers = {1,2};
-            defaultTable.addRow(new int[][]{integers});
         }
 
     }
 
     private void addUserButtonPressed(Bill bill) {
+        addUserButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                AddUserPage addUserPage = new AddUserPage(bill);
+                addUserPage.setVisible(true);
+                dispose();
+            }
+        });
     }
 
     private void removeUserButtonPressed(Bill bill) {
     }
 
     private void backButtonPressed() {
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                MainMenuPage mainMenuPage = new MainMenuPage(bill);
+                mainMenuPage.setVisible(true);
+                dispose();
+            }
+        });
     }
 
-
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
-        initializeTable();
-    }
 }
